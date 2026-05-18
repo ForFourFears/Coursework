c0ddc7ba746f56f449d7b5ce39dfe8b4d6ebf95d
For Four Fears <wotkot.gpn@gmail.com>
2026-05-18 16:54:42 +0300

    point1

---
+++ Assets/Scripts/LogicControllers/ModifierSystems/ModefierSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coursework.LogicControllers.ModifierSystems
+{
+    public class ModifierSystem
+    {
+        private readonly float baseModifier;
+        public float StateModifier;
+        public List<float> EffectsModifiers;
+
+        public ModifierSystem(float baseModifier)
+        {
+            this.baseModifier = baseModifier;
+            StateModifier = 1;
+            EffectsModifiers = new();
+        }
+
+        public float ApllyModifiers()
+        {
+            float result = baseModifier * StateModifier;
+            if (EffectsModifiers.Count != 0)
+            {
+                foreach (float modifier in EffectsModifiers)
+                {
+                    result += modifier;
+                }
+            }
+            return result;
+        }
+
+    }
+}
